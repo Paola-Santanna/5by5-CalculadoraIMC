82089041ae33547cb8525870a1337ff432842cab
Paola-Santanna <paolinhasj@gmail.com>
2024-04-21 12:31:05 -0300

    #Estrutura Básica Foram desenvolvidas as estruturas básicas do código, como a entrada de dados e os laços de repetição, para exigir que o usuário digite o valor correto.

--- Calculadora de IMC/Program.cs
+++ Calculadora de IMC/Program.cs
@@ -1,6 +1,36 @@
 ﻿//Faça um programa que leia o peso, a altura e o sexo de uma pessoa e calcule o seu IMC, informando a situação da pessoa: sobrepeso, .... assim vai
 
 //Início
+Console.WriteLine("--- Calculadora de IMC ---\n");
+
 //Declaração de variáveis
+float imc, peso, peso_aux, altura, altura_aux;
+
 //Entrada de Dados e Operações
+Console.WriteLine("Digite um valor POSITIVO e DIFERENTE DE ZERO\n");
+
+do
+{
+    Console.Write("Peso, em kg: ");
+    peso = float.Parse(Console.ReadLine());
+    peso_aux = peso;
+
+    if (peso_aux == 0 || peso_aux < 0)
+        Console.WriteLine("\nNúmero Inválido\n");
+
+} while (peso_aux == 0 || peso_aux < 0);
+
+do
+{
+    Console.Write("Altura, em metros: ");
+    altura = float.Parse(Console.ReadLine());
+    altura_aux = altura;
+
+    if (altura_aux == 0 || altura_aux < 0)
+        Console.WriteLine("\nNúmero Inválido\n");
+
+} while (altura_aux == 0 || altura_aux < 0);
+
 //Final do Programa
+Console.WriteLine("\nAperte ENTER para encerrar...");
+Console.ReadKey();
