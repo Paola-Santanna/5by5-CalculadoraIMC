56078e1d393887f860fd3c5a001bc45e9fd08431
Paola-Santanna <paolinhasj@gmail.com>
2024-04-21 13:00:37 -0300

    #Operações
    O desenvolvimento das operações contou com o uso da biblioteca System.Math, para a potenciação.
    Para o uso dessa função, foi necessário mudar as variáveis para double.
    Também, foi adicionada a estrutura de condição, para a classificação do IMC.

--- Calculadora de IMC/Program.cs
+++ Calculadora de IMC/Program.cs
@@ -7,7 +7,8 @@ Console.WriteLine("--- Calculadora de IMC ---\n");
 float imc, peso, peso_aux, altura, altura_aux;
 
 //Entrada de Dados e Operações
-Console.WriteLine("Digite um valor POSITIVO e DIFERENTE DE ZERO\n");
+Console.WriteLine("Digite um valor POSITIVO e DIFERENTE DE ZERO");
+Console.WriteLine("Para números decimais, USE A VÍRGULA\n");
 
 do
 {
@@ -30,6 +31,51 @@ do
         Console.WriteLine("\nNúmero Inválido\n");
 
 } while (altura_aux == 0 || altura_aux < 0);
+
+imc = peso_aux / (altura_aux * altura_aux);
+
+if (imc <= 18.5)
+{
+    Console.WriteLine($"\nIMC: {imc}");
+    Console.WriteLine("Magreza");
+}
+else
+{
+    if (imc >= 18.6 && imc <= 24.9)
+    {
+        Console.WriteLine($"\nIMC: {imc}");
+        Console.WriteLine("Peso Normal");
+    }
+    else
+    {
+        if (imc >= 25 && imc <= 29.9)
+        {
+            Console.WriteLine($"\nIMC: {imc}");
+            Console.WriteLine("Sobrepeso");
+        }
+        else
+        {
+            if (imc >= 30 && imc <= 34.9)
+            {
+                Console.WriteLine($"\nIMC: {imc}");
+                Console.WriteLine("Obesidade de grau I");
+            }
+            else
+            {
+                if (imc >= 35 && imc <= 39.9)
+                {
+                    Console.WriteLine($"\nIMC: {imc}");
+                    Console.WriteLine("Obesidade de grau II");
+                }
+                else
+                {
+                    Console.WriteLine($"\nIMC: {imc}");
+                    Console.WriteLine("Obesidade de grau III");
+                }
+            }
+        }
+    }
+}
 
 //Final do Programa
 Console.WriteLine("\nAperte ENTER para encerrar...");
