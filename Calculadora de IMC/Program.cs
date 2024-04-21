2f945e0ed3649a8859e5c5dcc51e997030338e93
Paola-Santanna <paolinhasj@gmail.com>
2024-04-21 12:14:45 -0300

    Add project files.

---
+++ Calculadora de IMC/Program.cs
@@ -0,0 +1,6 @@
+﻿//Faça um programa que leia o peso, a altura e o sexo de uma pessoa e calcule o seu IMC, informando a situação da pessoa: sobrepeso, .... assim vai
+
+//Início
+//Declaração de variáveis
+//Entrada de Dados e Operações
+//Final do Programa
